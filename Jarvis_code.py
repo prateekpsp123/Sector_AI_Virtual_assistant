c82663987631991bcba91c1a8e1d18e5b94cf489
Prateek Singh <prateekpsp123@gmail.com>
2023-04-13 23:20:43 +0530

    Added code for taking the command and searching on wikkipedia

--- Jarvis_code.py
+++ Jarvis_code.py
@@ -1,5 +1,7 @@
 import pyttsx3
+import speech_recognition as sr
 import datetime
+import wikipedia
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -13,13 +15,37 @@ def speak(audio):
 def wishMe():
     hour = int(datetime.datetime.now().hour)
     if hour > 0 and hour < 12:
-        speak("Good Morning Prateek sir")
+        speak("Good Morning Prateek sir, Hope you are doing well, tell me what can i do for you ?")
     elif hour > 12 and hour < 18:
-        speak("Good evening Prateek sir")
+        speak("Good evening Prateek sir, Hope you are doing well, tell me what can i do for you ?")
     else:
-        speak("Good evening Prateek sir")       
+        speak("Good evening Prateek sir, Hope you are doing well, tell me what can i do for you ?")       
+
+def take_Command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...........")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing.........")   
+        query = r.recognize_google(audio, language='en-in') 
+        print(f"user said: {query}")
+    except Exception as e:
+        print("Please say that again sir !")   
+        return "None"
+    return query 
 
 if __name__ == "__main__":
     wishMe()
-    speak("Hope you are doing well, tell me what can i do for you ?")    
-    
+    while True:
+        query = take_Command().lower()
+
+        #Logic for executing the task 
+        if "what" in query or "who" in query or "tell" in query or "which" in query: 
+            speak("sure sir, searching about this")
+            query = query.replace("wikipedia","")
+            result = wikipedia.summary(query, sentences=2)
+            speak("This is what i found !")
+            speak(result)
